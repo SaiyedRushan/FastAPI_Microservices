effe1793cb6aa82689dce4777f989a0f9a77b89a
SaiyedRushan <rushan52@gmail.com>
2024-05-17 16:16:35 -0400

    add router

--- inventory-frontend/src/App.js
+++ inventory-frontend/src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+// import {Products} from "./components/Products";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+// import {ProductsCreate} from "./components/ProductsCreate";
+import {Orders} from "./components/Orders";
 
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+    return <BrowserRouter>
+        <Routes>
+            {/* <Route path="/" element={<Products/>}/>
+            <Route path="/create" element={<ProductsCreate/>}/> */}
+            <Route path="/orders" element={<Orders/>}/>
+        </Routes>
+    </BrowserRouter>;
 }
 
 export default App;
